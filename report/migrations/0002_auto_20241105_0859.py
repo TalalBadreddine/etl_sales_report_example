120b57f1549a9ec34fe300d17c6767e7f95b15ff
talal <talalbadreddine@hotmail.com>
2024-11-05 11:04:17 +0200

    fixing migrations dependendies

--- report/migrations/0002_auto_20241105_0859.py
+++ report/migrations/0002_auto_20241105_0859.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('report', '0004_alter_report_customer_id'),
+        ('report', '0001_initial'),
     ]
 
 
